1d732cd2b58622f17e08fac43df5bfdd93592563
Courtney Garcia <wavesofgrace3@gmail.com>
2022-04-04 17:06:21 -0700

    commit tree 7b07b0d4fc42a055ef0ac890458a794d60756bee

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  if (ride <= 400) {
+    return 'This one is on me!'
+}   else if (ride > 400 && ride < 2000) {
+      return 'That will be twenty bucks.'
+}   else if (ride < 2500 && ride > 2000) {
+      return 'I will gladly take your thirty bucks.'
+}   else if (ride > 2000 && ride < 2500); {  
+      return 'No can do.' 
+  }   
+}   
+
+function ternaryCheckCity(city){
+  if (city === 'NYC') {
+    return 'Ok, sounds good.';
+  }
+  else {
+    return 'No go.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  if (tip === 'generous') {
+    return 'Thank you so much.';
+  }
+  else if (tip === 'not as generous') {
+    return 'Thank you.';
+  }
+  else {
+    return 'Bye.';
+  }
 }
